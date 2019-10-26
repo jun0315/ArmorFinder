5c846612c184be629d49f34fc8fd181cb1676f38
jun0315 <1072505283@qq.com>
2019-10-26 22:32:59 +0800

    remove unneeded files

--- src/xqj/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xqj/catkin_ws/src/xqj
-# Build directory: /home/xqj/catkin_ws/src/xqj/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
